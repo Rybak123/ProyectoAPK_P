71cb8bbdadc9671f7220e715158b0299965b8349
kellyAyD <gutierrezkelly233@gmail.com>
2021-09-05 01:36:26 -0300

    VersionWebActualizada
    Actualizacion de la version de la interfaz web del control de estudio

--- PlataformaVirtualHumanos/src/app/paciente/cuerpo/control-de-estudio/control-de-estudio.component.ts
+++ PlataformaVirtualHumanos/src/app/paciente/cuerpo/control-de-estudio/control-de-estudio.component.ts
@@ -1,43 +1,48 @@
-import { OnInit } from '@angular/core';
+import { OnInit,Input,EventEmitter } from '@angular/core';
 import { Component,ViewEncapsulation } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/angular';
+import { CalendarOptions, EventInput } from '@fullcalendar/angular';
 @Component({
   selector: 'app-control-de-estudio',
   templateUrl: './control-de-estudio.component.html',
   styleUrls: ['./control-de-estudio.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class ControlDeEstudioComponent implements OnInit {
-
-  
+export class ControlDeEstudioComponent implements OnInit{
   constructor() { }
-
+  calendarVisible = true;
   ngOnInit(): void {
-    this.iniciarCalendario(controlDeEstudio);
+    controlDeEstudio.forEach( element=>{
+    console.log(element);
+       var mydate = new Date(element.fecha.trim());
+       var descripcion= "Tiempo: "+element.cantidadDeTiempo+'\n'+" Materia: "+element.materia;
+      
+       this.calendarEvents= this.calendarEvents.concat({ 
+         title: descripcion,
+         start: mydate
+      })
+   });
+   this.calendarOptions.events=this.calendarEvents;
+  //  this.iniciarCalendario(controlDeEstudio);
   }
+  calendarEvents: EventInput[] = [ 
+  ];
   title = 'practica1';
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     dateClick: this.handleDateClick.bind(this),
-    events: [
-      { title: 'event 1', date: '2020-06-27' },
-      { title: 'event 2', date: '2020-06-30' }   
+    events: [   
     ]
   };
   handleDateClick(arg:any) {
     alert('date click! ' + arg.dateStr)
   }
-
-  iniciarCalendario(controlDeEstudioJson:any){
-    
-  }
-  
-
+  //iniciarCalendario(controlDeEstudioJson:any){
+  //}
 }
 var controlDeEstudio=[
-  {fecha:"2021-03-08T04:00:00.000+00:00",cantidadDeTiempo:3,materia:"Matematicas"},
-  {fecha:"2021-04-08T04:00:00.000+00:00",cantidadDeTiempo:6,materia:"Lenguaje"},
-  {fecha:"2021-05-08T04:00:00.000+00:00",cantidadDeTiempo:9,materia:"Fisica"},
+  {fecha:"2021-09-03T04:00:00.000+00:00",cantidadDeTiempo:3,materia:"Matematicas"},
+  {fecha:"2021-09-04T04:00:00.000+00:00",cantidadDeTiempo:6,materia:"Lenguaje"},
+  {fecha:"2021-09-05T04:00:00.000+00:00",cantidadDeTiempo:9,materia:"Fisica"},
 ]; 
 
 
