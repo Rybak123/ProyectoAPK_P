fe26273cbd689a7b3e05ed5dc89872a4de3c536a
amosjoel <amo.ajpf@gmail.com>
2021-09-04 21:36:18 -0400

    Cronometro agregado

--- PlataformaVirtualHumanos/src/app/paciente/cuerpo/control-de-estudio/control-de-estudio.component.ts
+++ PlataformaVirtualHumanos/src/app/paciente/cuerpo/control-de-estudio/control-de-estudio.component.ts
@@ -8,9 +8,54 @@ import { CalendarOptions } from '@fullcalendar/angular';
   encapsulation: ViewEncapsulation.None
 })
 export class ControlDeEstudioComponent implements OnInit {
+  public hora:number = 0;
+  public minutos:number = 0;
+  public segundos:number = 0;
 
-  
+  /*public horalapso: number;
+  public minutoslapso: number;
+  public segundoslapso: number;*/
+ /* public coleccion:Array<any> =[];*/
+  public contador:any;
+
   constructor() { }
+
+  start(){ 
+    if(this.contador == undefined){ 
+      this.contador = setInterval(()=>{ 
+        this.segundos += 1;
+        if(this.segundos == 60){ 
+          this.segundos = 0;
+          this.minutos +=1;
+          if(this.minutos == 60){ 
+            this.minutos = 0;
+            this.hora +=1;
+            if(this.hora = 24){ 
+              this.hora = 0;
+            }
+          }
+        }
+      }
+      ,1000);
+    }
+
+  }
+  /*lapso(){ 
+    let obj:any = {};
+    obj.hora = this.hora;
+    obj.minutos = this.minutos;
+    obj.segundos = this.segundos;
+
+    this.coleccion.push(obj);
+  }*/
+
+  stop(){ 
+    clearInterval(this.contador);
+    this.hora = 0;
+    this.minutos = 0;
+    this.segundos = 0;
+    this.contador = null;
+  }
 
   ngOnInit(): void {
     this.iniciarCalendario(controlDeEstudio);
--- Proyecto_APK_Movil/app/src/main/java/com/example/proyecto_apk/MainActivity.java
+++ Proyecto_APK_Movil/app/src/main/java/com/example/proyecto_apk/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.proyecto_apk;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -12,7 +16,15 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.proyecto_apk.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button3,button5;
+    boolean isOn= false;
+    TextView textView3;
+    Thread cronos;
+    int mili=0, segu=0, minutos=0;
+    Handler h=new Handler();
+
 
     private ActivityMainBinding binding;
 
@@ -32,6 +44,74 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
 
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        setContentView(R.layout.fragment_control_de_estudio);
+        button3=(Button)findViewById(R.id.button3);
+        button5=(Button)findViewById(R.id.button5);
+        textView3=(TextView)findViewById(R.id.textView3);
+        button3.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        cronos=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true){
+                    if (isOn){
+                        try {
+                            Thread.sleep(1);
+
+                        }catch (InterruptedException e){
+                            e.printStackTrace();
+                        }
+                        mili++;
+                        if (mili==999){
+                            segu++;
+                            mili=0;
+                        }
+                        if (segu==59){
+                            minutos++;
+                            segu=0;
+                        }
+                        h.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                String m="",s="",mi="";
+                                if (mili<10){
+                                    m="00"+mili;
+                                }else if (mili<100){
+                                    m="0"+mili;
+                                }else{
+                                    m=""+mili;
+                                }
+                                if (segu<10){
+                                    s="0"+segu;
+                                }else{
+                                    s=""+segu;
+                                }
+                                if (minutos<10){
+                                    mi="0"+minutos;
+                                }else{
+                                    mi=""+minutos;
+                                }
+                                textView3.setText(mi+":"+s+":"+m);
+                            }
+                        });
+                    }
+                }
+            }
+        });
+        cronos.start();
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button3:
+                isOn=true;
+                break;
+            case R.id.button5:
+                isOn=false;
+                break;
+        }
+
+    }
 }
